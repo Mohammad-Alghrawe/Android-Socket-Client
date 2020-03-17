7ae6bc90e596145c2e7040824cdad2a26fb19dc2
Mohammad <54815458+Mohammad-Alghrawe@users.noreply.github.com>
2020-03-17 01:12:54 +0100

    initial the tv and the btn and implement them to send a message

--- app/src/main/java/com/example/android_socket_client/MainActivity.java
+++ app/src/main/java/com/example/android_socket_client/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.android_socket_client;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText in_msg;
+    Button send_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        in_msg = findViewById(R.id.in_msg);
+        send_btn = findViewById(R.id.send_btn);
+
+        send_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new SendMessage().execute(in_msg.getText().toString());
+                in_msg.getText().clear();
+            }
+        });
+
+
     }
+
+
 }
